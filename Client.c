c8f7d3a4ef7c1496bbbdf063716cc099cc693684
Joshimore <egorladur@gmail.com>
2026-07-29 16:02:20 +0200

    task assign + basic features

---
+++ Client.c
@@ -0,0 +1,13 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+
+int main(){
+
+   int connector;
+
+   connector = socket(AF_INET, )
+
+}
+
+
---
+++ Server.c
@@ -0,0 +1,19 @@
+/*server*/
+
+/*STRUCT TO DO: 
+
+  SOCKET() -> BIND() -> LISTEN() -> ACCEPT()
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+
+
+
+
+
