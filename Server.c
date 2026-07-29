04f82e95f3a27ccd4bb06e79dd798e7d76959e95
Joshimore <egorladur@gmail.com>
2026-07-29 23:33:05 +0200

    .gitignore

--- Server.c
+++ Server.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -16,11 +17,28 @@
 
 int main(){
 
-    int connector = socket(
-            AF_INET, 
-            SOCK_STREAM, 
-            0
-        )
+    int connector; 
+    struct sockaddr_in name;
+
+    //socket initialization
+    if((connector = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+
+       perror("Socket malfunction"); 
+       return 0;
+
+    }
+
+    memset(&name, 0, sizeof(name));
+
+    name.sin_family = AF_INET;
+    name.sin_port = htons(9999); 
+    name.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+
+//    listen()
+
+//    accept()
 
 
 
